ac8fb2f8cd4d6d4d8fc57a9019c007cf992ff280
Raviteja4827 <141260420+Raviteja4827@users.noreply.github.com>
2025-04-19 11:18:26 +0530

    Create BaloonValidation.java

---
+++ src/balloonValidation/BaloonValidation.java
@@ -0,0 +1,23 @@
+package opps_programming_optimizing_style2;
+
+public class BaloonValidation {
+public static boolean baloonColourValidation(String baloonColour) {
+if(baloonColour.equalsIgnoreCase("Green")||
+baloonColour.equalsIgnoreCase("Yellow")||
+baloonColour.equalsIgnoreCase("White")||
+baloonColour.equalsIgnoreCase("Gold")||
+ baloonColour.equalsIgnoreCase("Red")){
+	return true;
+}
+else {
+	return false;
+}}
+public static boolean baloonPriceValidation(double baloonPrice){
+	if(baloonPrice>=2 && baloonPrice<=15) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+}
