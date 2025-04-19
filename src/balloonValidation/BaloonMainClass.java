f523a7cc80476b85a6d91e2c8d1d84c225fd97f3
Raviteja4827 <141260420+Raviteja4827@users.noreply.github.com>
2025-04-19 11:18:25 +0530

    Create BaloonMainClass.java

---
+++ src/balloonValidation/BaloonMainClass.java
@@ -0,0 +1,10 @@
+package opps_programming_optimizing_style2;
+
+public class BaloonMainClass {
+public static void main(String args[]) {
+	Baloon b1=new Baloon("Yellow",4.5);
+    b1.Display();
+    Baloon b2=new Baloon("Pink",11.0);
+    b2.Display();
+}
+}
